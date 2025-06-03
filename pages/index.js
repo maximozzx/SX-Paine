51322dc6fe85c5b21999ffeef175fa734d67d561
maximozzx <thiagotulerramos@gmail.com>
2025-06-03 18:22:02 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
+      <h1>:SX | Bot - Painel de Criação de Embeds</h1>
+      <p>Bem-vindo ao painel! Aqui você poderá criar e editar embeds para o seu bot.</p>
+      {/* Aqui futuramente terá o editor de embed */}
+    </div>
+  )
+}
